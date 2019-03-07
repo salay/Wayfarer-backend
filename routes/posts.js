068fcd4fba049aa65896c59b65a2bccb73f4d711
lfonzi62 <lfontenette@berkeley.edu>
2019-03-06 20:31:11 -0800

    updated format of posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,15 +3,10 @@ const
   router = express.Router(),
   controllers = require('../controllers')
 
-<<<<<<< HEAD
 router.get('/', controllers.posts.index)
 router.post('/',controllers.posts.newpost)
 router.delete('/',controllers.posts.deletePost)
 router.put('/',controllers.posts.updatePost)
-=======
-router.get('/', controllers.posts.index);
-// router.post('/', controllers.posts.postCreation);
->>>>>>> df0ad9a34da28a232e163f655d383d14acd5a50b
 
 //router.post('/posts', controllers.posts.posts);
 //delte
